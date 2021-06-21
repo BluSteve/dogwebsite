d27731342738c67660f86aa96e7aab2f0b4412fa
BluSteve <billiegatesftw@gmail.com>
2021-06-21 12:50:50 +0800

    Minor aesthetic changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar
-        app
-        color="primary"
-    >
+    <v-app-bar app color="primary">
       <div class="d-flex align-center">
         <router-link to="/">
           <v-img
               class="shrink mr-2"
               contain
-              src="https://www.telegraph.co.uk/content/dam/technology/2021/01/28/Screenshot-2021-01-28-at-13-20-35_trans_NvBQzQNjv4BqEGKV9LrAqQtLUTT1Z0gJNRFI0o2dlzyIcL3Nvd0Rwgc.png"
+              :src="require('@/assets/doge.jpg')"
               width="70"
           />
         </router-link>
       </div>
       <v-spacer></v-spacer>
       <router-link :to="inSettings ? '/' : '/settings'">
-        <v-btn @click="inSettings = !inSettings" class="mx-1">
+        <v-btn class="mx-1" @click="inSettings = !inSettings">
           <v-icon>mdi-cog-outline</v-icon>
         </v-btn>
       </router-link>
     </v-app-bar>
     <br>
-    <v-main><router-view/></v-main>
+    <v-main>
+      <router-view/>
+    </v-main>
   </v-app>
 </template>
 
